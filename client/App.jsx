377093c5c6d71282ae56f6e2553c2e82df0d16a1
Jenny <jennyoh1216@gmail.com>
2023-03-11 15:16:49 -0500

    signin/signup created, background and some css added

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,10 @@
-import React from "react";
+import React from 'react';
+import LSContainer from './Components/LoginSignup/LSContainer';
 
 const App = () => {
-    //THIS IS A FUNCTION JUST FOR TESTING IF THE BACKEND AND FRONTEND IS CONNECTED
-  const onclick = () => {
-    fetch("/test").then(console.log("testing"));
-  };
   return (
     <React.Fragment>
-      <h2>Hello!</h2>
-      <button onClick={onclick}>test frontend and backend connection</button>
+      <LSContainer />
     </React.Fragment>
   );
 };
--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./App";
+import styles from "./styles/Application.scss";
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
