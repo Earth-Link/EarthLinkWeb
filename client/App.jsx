90cbfdf85f961f79ad7c11905c5f3b8501eacc13
Junaid Ahmed <junaid7ahmed96@gmail.com>
2023-03-10 20:19:06 -0500

    boilerplate done and webpack done, frontend and backend connected

---
+++ client/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+    //THIS IS A FUNCTION JUST FOR TESTING IF THE BACKEND AND FRONTEND IS CONNECTED
+  const onclick = () => {
+    fetch("/test").then(console.log("testing"));
+  };
+  return (
+    <React.Fragment>
+      <h2>Hello!</h2>
+      <button onClick={onclick}>test frontend and backend connection</button>
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+//HANDLE PARSING REQUEST BODIES
+app.use(express.json());
+
+//JUST A TEST ROUTE
+app.get('/test', (req, res, next) => {
+    console.log('in backend');
+    res.status(200).send('COMPLETE');
+})
+
+//GLOBAL ERROR HANDLING
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: "Express error handler caught unknown error",
+    status: 400,
+    message: { err: "An error occurred" },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+//STARTING THE BACKEND SERVER
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
